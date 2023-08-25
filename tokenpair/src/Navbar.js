fed28aeca9b341b7502d39bcfec7e86fa9dd9aac
harshitabhatia26 <bharshita2604@gmail.com>
2023-08-25 14:43:27 +0530

    left navbar created

---
+++ tokenpair/src/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return(
+<nav class="nav rounded-right flex-column">
+  <a class="nav-link text-center p-3" aria-current="page" href="#">Token Address</a>
+  <a class="nav-link text-center p-3" aria-current="page" href="#">Pair Address</a>
+</nav>
+    )
+}
+
+export default Navbar;
